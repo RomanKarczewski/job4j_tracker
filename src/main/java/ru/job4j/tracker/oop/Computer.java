2da2c7dc8f9ec3ebcdb629c83e6e789c1ab9c6b3
Roma <theta.box@gmail.com>
2021-08-04 01:57:53 +0200

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/oop/Computer.java
+++ src/main/java/ru/job4j/tracker/oop/Computer.java
@@ -14,6 +14,47 @@ public class Computer {
         this.cpu = cpu;
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer() {
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd + " GB");
+        System.out.println("Модель CPU: " + cpu);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
+    }
+}
+
+/* public class Computer {
+
+    private boolean multiMonitor;
+
+    private int ssd;
+
+    private String cpu;
+
+    public Computer(boolean multiMonitor, int ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public Computer() {
     }
 
@@ -29,4 +70,5 @@ public class Computer {
         Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
         comp.printInfo();
     }
-}
+
+ */
