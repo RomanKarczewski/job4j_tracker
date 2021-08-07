29cd9636b81ba9cc6d66bd1907a6421b267bfe32
Roma <theta.box@gmail.com>
2021-08-07 22:59:40 +0200

    4. Переопределение [#174890]

--- src/main/java/ru/job4j/tracker/oop/Doctor.java
+++ src/main/java/ru/job4j/tracker/oop/Doctor.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker.oop;
 
 public class Doctor extends Profession {
+    private String diagnosis;
+
     public Doctor () {
     }
 
     Pacient Vaso = new Pacient();
-
+    
     public String diagnosisHeal(Pacient pacient) {
         return this.diagnosis;
     }
